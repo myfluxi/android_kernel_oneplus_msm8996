4b5fde186206577fd45c001b0d0c6f7e1cac2c47
fluxi <linflux@arcor.de>
2016-10-09 11:44:19 +0200

    cpu: Remove OEM changes
    Change-Id: If9661eee98498b487be491ad0486b10ead3e18d7

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -22,7 +22,6 @@
 #include <linux/lockdep.h>
 #include <linux/tick.h>
 #include <trace/events/power.h>
-#include <linux/sched.h>
 
 #include "smpboot.h"
 
@@ -406,8 +405,6 @@ out_release:
 	cpu_hotplug_done();
 	if (!err)
 		cpu_notify_nofail(CPU_POST_DEAD | mod, hcpu);
-
-	pr_debug("%s by %s[%d]\n", __func__, current->comm, current->pid);
 	return err;
 }
 
@@ -560,7 +557,6 @@ int disable_nonboot_cpus(void)
 	cpumask_clear(frozen_cpus);
 
 	pr_info("Disabling non-boot CPUs ...\n");
-	sched_set_boost(0);//Wujialong 20160314 disable sched_boost when going to sleep
 	for_each_online_cpu(cpu) {
 		if (cpu == first_cpu)
 			continue;
